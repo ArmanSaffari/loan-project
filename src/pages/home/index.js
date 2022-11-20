46dff6c1c207391d80becbcc816f03210521a70c
Arman Saffari <a.saffari256@gmail.com>
2022-11-20 13:27:35 -0500

    style of Home page modified

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,55 @@
 import { Link } from "react-router-dom";
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Grid } from '@mui/material';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 
 
 function Home() {
     return (
         <>
-            <div className="loginForm">
-                <form>
-                    <TextField id="outlined-basic" label="User Name" variant="outlined" />
-                    <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" />
-                    <Button variant="outlined" startIcon={<LoginOutlinedIcon />}>Login</Button>
-                    <Link to="/Register">Register</Link>
-                    <Link to="/forgetPassword">Forget Password</Link>
-                </form>
+            <div>
+                <Grid container
+                    spacing={0}
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        margin: 'auto'
+                    }}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <TextField
+                            id="outlined-basic"
+                            label="User Name"
+                            variant="outlined"
+                            sx={{
+                                width: '100%',
+                                margin: '20px'
+                            }}
+                            color="success"/>
+                        <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            sx={{
+                                width: '100%',
+                                margin: '20px'
+                            }}
+                            color="success"/>
+                        <Button
+                            variant="outlined"
+                            startIcon={<LoginOutlinedIcon />}
+                            sx={{
+                                width: '100%',
+                                margin: '20px'
+                            }}
+                            color="success">Login</Button>
+                        <div style={{margin: '20px'}}>
+                            <Link to="/Register">Register</Link>
+                        </div>
+                        <div style={{margin: '20px'}}>
+                            <Link to="/forgetPassword">Forget Password</Link>
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
         </>
     )
