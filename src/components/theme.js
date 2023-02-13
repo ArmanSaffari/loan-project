47132f9f242c810d2108328873b72ca5a7122d25
ArmanSaffari <a.saffari256@gmail.com>
2023-02-12 20:35:32 -0500

    styles added to all pages

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,11 +1,39 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const goldenColor = '#E8C45F';
-const darBlueColor = '#1B307D';
+const darkBlueColor = '#1B307D';
 const lightGrayColor = '#C2C2C2';
 const darkGrayColor = '#1E1E1E';
 const blueColor = '#058BE8';
 const whiteColor = '#FFFFFF';
+
+export const appTheme = createTheme({
+
+  palette: {
+    // mode: 'light',
+    success: {
+      light: goldenColor,
+      main: goldenColor,
+      dark: goldenColor,
+    //   contrastText: 'rgba(0, 0, 0, 0.87)',
+    },
+    info: {
+      light: blueColor,
+      main: blueColor,
+      dark: blueColor,
+    },
+    primary: {
+      light: darkGrayColor,
+      main: darkGrayColor,
+      dark: darkGrayColor,
+    },
+    secondary: {
+      light: darkBlueColor,
+      main: darkBlueColor,
+      dark: darkBlueColor,
+    }
+  },
+});
 
 export const loginTheme = createTheme({
   palette: {
@@ -15,9 +43,11 @@ export const loginTheme = createTheme({
       main: goldenColor,
       dark: goldenColor,
       contrastText: 'rgba(0, 0, 0, 0.87)',
-      }
+      },
   },
-  typography: {},
+  typography: {
+    
+  },
   components: {
     // Name of components
     MuiFormControl: {
@@ -84,8 +114,6 @@ export const loginTheme = createTheme({
     MuiContainer: {
       styleOverrides: {
         root: {
-
-
           '&.mainFormContainer': {
             padding: '0px',
             height: '100vh',
@@ -169,19 +197,286 @@ export const loginTheme = createTheme({
 
 export const navbarTheme = createTheme({
   palette: {
-    // mode: 'light',
     success: {
       light: goldenColor,
       main: goldenColor,
       dark: goldenColor,
       contrastText: 'rgba(0, 0, 0, 0.87)',
-      }
+    },
+    info: {
+      light: blueColor,
+      main: blueColor,
+      dark: blueColor,
+    },
+    primary: {
+      light: darkGrayColor,
+      main: darkGrayColor,
+      dark: darkGrayColor,
+    },
+    secondary: {
+      light: darkBlueColor,
+      main: darkBlueColor,
+      dark: darkBlueColor,
+    }
   },
+
+  typography: {
+    root: {
+    },
+  },
+
   components: {
-    // modifying buttons
+
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: `${goldenColor}80`,
+          position: "static",
+        },
+      },
+    },
+
+    // modifying toolbar
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          'img': {
+            '&.navbarLogo': {
+              height: '40px',
+              marginRight: '20px',
+            },
+          },
+        },
+      },
+    },
+
     MuiButton: {
       styleOverrides: {
         root: {
+          '&.navbarMenuButtons': {
+            display: 'block',
+            color: darkGrayColor,
+            fontSize: '0.8em',
+            margin: '0px 20px',
+          },
+
+          '&.navbarMenuItem': {
+            color: darkBlueColor,
+            minWidth: '300px',
+            fontSize: '0.8em',
+            margin: '0px',
+            letterSpacing: '0.2em',
+            '&:hover': {
+              fontWeight: 'bold',
+              letterSpacing: '0.5em'
+            }
+          }
+        },
+      },
+    },
+
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          '&.navbaContainer': {
+            height: '40px',
+          },
+        },
+      },
+    },
+    
+    MuiGrid: {
+      styleOverrides: {
+        root: {
+          'div.userInfo': {
+            textAlign: 'center',
+            margin: '4px 0px',
+          }
+        },
+      },
+    },
+
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          '&.avatarContainer': {
+            display: 'flex',
+            border: `4pt solid ${goldenColor}`,
+            width: '50px',
+            height: '50px',
+            position: 'absolute',
+            top: '15px',
+            right: '0px',
+            boxShadow:  `0 4px 4px ${darkBlueColor}80`
+          },
+
+          '&.navbarMenuButton': {
+          }
+        },
+      },
+    },
+
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+        },
+      },
+    },
+
+  },
+});
+
+export const dashboardTheme = createTheme({
+
+  components: {
+
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          '&.dashboardContainer': {
+            
+            minHeight: '100vh',
+            background: '#ffffff url("./images/backgroundImage-dashboard.png") no-repeat center top',
+            backgroundSize: 'cover',
+            backgroundClip: "border-box",
+
+            'h3.header': {
+              color: darkBlueColor,
+            }
+          },
+
+          '&.cardTitleBar': {
+            border: '1pt solid red'
+          },
+        },
+      },
+    }, 
+
+    MuiGrid: {
+      styleOverrides: {
+        root: {
+          '&.cardTitleBar': {
+            position: 'relative',
+            width: '100%',
+            minHeight: '60px',
+            background: `${goldenColor}80`,
+            alignItems: 'center',
+          },
+
+          '&.linkButtonContainer': {
+            position: 'absolute',
+            right: '0',
+            bottom: '0',
+            width: '40px',
+            height: '40px',
+            border: `4pt solid ${goldenColor}`,
+          },
+
+          '&.CardContentContainer': {
+            width: "100%",
+            margin: '60px 0px',
+            color: darkBlueColor,
+            textShadow: `2px 2px 4px ${darkBlueColor}`,            
+          },
+          
+        },
+      },
+    }, 
+
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          '&.dashboardCard': {
+            backgroundColor: `${whiteColor}80`,
+            boxShadow:  `2px 2px 4px ${darkBlueColor}`,
+          },
+
+          'div.cardTitle': {
+            color: darkGrayColor,
+            padding: '10px',
+            display: 'flex',
+            fontSize: '1.2rem',
+            fontWeight: 'bold'
+          },
+
+          'div.cardContent': {
+            fontSize: '4rem',
+
+            textAlign: 'center',
+          },
+        },
+      },
+    },
+
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          '&.linkButton': {
+            position: 'absolute',
+            right: '5px',
+            bottom: '65px',
+            width: '40px',
+            height: '40px',
+            border: `4pt solid ${goldenColor}`,
+          },
+        },
+      },
+    },
+
+    MuiAccordionSummary: {
+      styleOverrides: {
+        root: {
+          backgroundColor: lightGrayColor,
+          color: darkBlueColor,
+          borderBottom: '1px solid gray',
+        },
+      },
+    },
+
+    MuiAccordion: {
+      styleOverrides: {
+        root: {
+          boxShadow: `2px 2px 4px ${darkBlueColor}`,
+        },
+      },
+    },
+
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: whiteColor,
+          height: '30px',
+          margin: '10px 0px',
+          color: darkBlueColor,
+          fontSize: '1em',
+          '&:hover': {
+            background: darkBlueColor,
+            color: whiteColor,
+            borderColor: goldenColor,
+            boxShadow: `0px 0px 10px ${goldenColor}`
+          },
+        },
+      },
+    },
+  },
+});
+
+export const customTableTheme = createTheme({
+
+  components: {
+
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          '&.CustomTableHeader': {
+            fontWeight: 'bold',
+            backgroundColor: darkBlueColor,
+            color: whiteColor,
+          },
+
+          '&.cardTitleBar': {
+            border: '1pt solid red'
+          },
         },
       },
     },
